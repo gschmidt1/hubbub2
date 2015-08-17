25b6be3f66a8dec85917df35862ebb4edac07e60
gnonschmidt <gnonschmidt@Newbie>
2015-08-16 19:21:25 -0500

    8/12/2014 Glenn Schmidt Hubbub2

--- src/java/edu/acc/j2ee/hubbub2/User.java
+++ src/java/edu/acc/j2ee/hubbub2/User.java
@@ -5,19 +5,27 @@ import java.util.Date;
 import java.util.List;
 
 public class User implements java.io.Serializable {
+
     private String userName;
+    private String password;
+
     private Date joinDate;
     private List<Post> posts = new ArrayList<>();
-    
+
     public User(String userName, Date joinDate) {
         this.userName = userName;
         this.joinDate = joinDate;
     }
-    
-    public User() {}
+
+    public User() {
+    }
 
     public String getUserName() {
         return userName;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public Date getJoinDate() {
@@ -28,14 +36,18 @@ public class User implements java.io.Serializable {
         this.userName = userName;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public void setJoinDate(Date joinDate) {
         this.joinDate = joinDate;
     }
-    
+
     public List<Post> getPosts() {
         return posts;
     }
-    
+
     @Override
     public String toString() {
         return userName;
